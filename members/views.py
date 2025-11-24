ce45a64be0ecc9bf2522725e9d59b355a9150873
oraculo-nl <info@oraculo.nl>
2025-11-24 13:11:54 +0100

    database tabel maken, template maken

--- members/views.py
+++ members/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
+from .models import Member
 
 # Create your views here.
 def hello_view(request):
-    return HttpResponse("Hello, world.")
+    mymembers = Member.objects.all().values()
+    print(type(mymembers))
+    template = loader.get_template('members/myfirst.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
